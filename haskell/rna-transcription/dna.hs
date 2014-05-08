1cb1308d6bbce66f162caecad269c95cacfcc542
Nat Williams <nat.williams@gmail.com>
2014-05-07 20:50:08 -0500

    submitted dna.hs

--- haskell/rna-transcription/dna.hs
+++ haskell/rna-transcription/dna.hs
@@ -1,4 +1,10 @@
 module DNA (toRNA) where
 
   toRNA :: String -> String
-  toRNA = undefined
+  toRNA = fmap complement
+
+  complement :: Char -> Char
+  complement 'G' = 'C'
+  complement 'C' = 'G'
+  complement 'T' = 'A'
+  complement 'A' = 'U'
