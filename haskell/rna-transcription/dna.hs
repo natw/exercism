d208af0bcc84c04faf80ee6058029c2e227dd970
Nat Williams <nwilliams@leapfrogonline.com>
2014-05-08 11:25:03 -0500

    dedent

--- haskell/rna-transcription/dna.hs
+++ haskell/rna-transcription/dna.hs
@@ -1,10 +1,10 @@
 module DNA (toRNA) where
 
-  toRNA :: String -> String
-  toRNA = fmap complement
+toRNA :: String -> String
+toRNA = fmap complement
 
-  complement :: Char -> Char
-  complement 'G' = 'C'
-  complement 'C' = 'G'
-  complement 'T' = 'A'
-  complement 'A' = 'U'
+complement :: Char -> Char
+complement 'G' = 'C'
+complement 'C' = 'G'
+complement 'T' = 'A'
+complement 'A' = 'U'
