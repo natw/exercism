a0fbea3c42c512b1bdc8145b86cb674b0406f5fd
Nat Williams <nwilliams@leapfrogonline.com>
2014-05-08 11:24:01 -0500

    fix indentation

--- haskell/bob/bob.hs
+++ haskell/bob/bob.hs
@@ -1,24 +1,23 @@
-
 module Bob (responseFor) where
 
-  import Data.Char
+import Data.Char
 
-  responseFor :: String -> String
-  responseFor statement
-    | isNothing statement = "Fine. Be that way!"
-    | isYelling statement = "Woah, chill out!"
-    | isQuestion statement = "Sure."
-    | otherwise = "Whatever."
+responseFor :: String -> String
+responseFor statement
+  | isNothing statement = "Fine. Be that way!"
+  | isYelling statement = "Woah, chill out!"
+  | isQuestion statement = "Sure."
+  | otherwise = "Whatever."
 
-  isQuestion :: String -> Bool
-  isQuestion statement =
-    not (null statement) &&
-    last statement == '?'
+isQuestion :: String -> Bool
+isQuestion statement =
+  not (null statement) &&
+  last statement == '?'
 
-  isYelling :: String -> Bool
-  isYelling statement =
-    all isUpper filtered && not (null filtered)
-    where filtered = filter isAlpha statement
-    
-  isNothing :: String -> Bool
-  isNothing = all isSpace
+isYelling :: String -> Bool
+isYelling statement =
+  all isUpper filtered && not (null filtered)
+  where filtered = filter isAlpha statement
+  
+isNothing :: String -> Bool
+isNothing = all isSpace
