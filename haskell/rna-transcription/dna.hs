02ce0fa6edaab250854d0a9277bd8da7348eb1e2
Nat Williams <nat.williams@gmail.com>
2014-05-08 21:27:51 -0500

    less indentation and better error

--- haskell/rna-transcription/dna.hs
+++ haskell/rna-transcription/dna.hs
@@ -4,8 +4,9 @@ toRNA :: String -> String
 toRNA = fmap complement
 
 complement :: Char -> Char
-complement nuc = case nuc of 'G' -> 'C'
-                             'C' -> 'G'
-                             'T' -> 'A'
-                             'A' -> 'U'
-                             _ -> error "unrecognized nucleotide"
+complement nuc = case nuc of
+  'G' -> 'C'
+  'C' -> 'G'
+  'T' -> 'A'
+  'A' -> 'U'
+  _ -> error $ "unrecognized nucleotide: " ++ show nuc
