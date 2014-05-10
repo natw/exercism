caa0e74b8152e196fac2d9567ea0c9c791ed1b05
Nat Williams <nat.williams@gmail.com>
2014-05-09 21:14:53 -0500

    dedent

--- haskell/anagram/anagram.hs
+++ haskell/anagram/anagram.hs
@@ -1,13 +1,13 @@
 module Anagram (anagramsFor) where
 
-  import Data.Char (toLower)
-  import Data.List (sort)
+import Data.Char (toLower)
+import Data.List (sort)
 
-  anagramsFor :: String -> [String] -> [String]
-  anagramsFor word wordList = 
-    filter (\a -> isAnagram word a) wordList
+anagramsFor :: String -> [String] -> [String]
+anagramsFor word wordList = 
+  filter (\a -> isAnagram word a) wordList
 
-  isAnagram word1 word2 =
-    lowercase word1 /= lowercase word2 &&
-    sort (lowercase word1) == sort (lowercase word2)
-    where lowercase = fmap toLower
+isAnagram word1 word2 =
+  lowercase word1 /= lowercase word2 &&
+  sort (lowercase word1) == sort (lowercase word2)
+  where lowercase = fmap toLower
