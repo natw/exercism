b2a6f1da7f0affacb3e5106ab119dd6af299bf0e
Nat Williams <nat.williams@gmail.com>
2014-05-09 19:24:01 -0500

    beer

--- haskell/beer-song/beer.hs
+++ haskell/beer-song/beer.hs
@@ -1,7 +1,18 @@
 module Beer (sing, verse) where
 
 sing :: Int -> Int -> String
-sing = undefined
+sing a b = 
+  unlines (map verse (reverse [b..a]))
 
 verse :: Int -> String
-verse = undefined
+verse num
+  | num > 1 = bottles num ++ " of beer on the wall, " ++ bottles num ++ " of beer.\nTake one down and pass it around, " ++ bottles (num - 1) ++ " of beer on the wall.\n"
+  | num == 1 = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+  | num == 0 = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
+  | otherwise = error "Never fear, have a beer"
+
+
+bottles :: Int -> String
+bottles num = case num of
+  1 -> "1 bottle"
+  _ -> show num ++ " bottles"
