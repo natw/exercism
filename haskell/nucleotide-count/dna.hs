5e1f10fba9082da04728850f74b9af08a1a32c84
Nat Williams <nat.williams@gmail.com>
2014-05-11 14:15:44 -0500

    remove more parens

--- haskell/nucleotide-count/dna.hs
+++ haskell/nucleotide-count/dna.hs
@@ -5,8 +5,10 @@ import qualified Data.Map.Strict as M
 count :: Char -> String -> Int
 count nuc
   | nuc `elem` "GATCU" = length . filter (nuc ==)
-  | otherwise = error ("invalid nucleotide " ++ show nuc )
+  | otherwise = error ("invalid nucleotide " ++ show nuc)
 
 nucleotideCounts :: String -> (M.Map Char Int)
 nucleotideCounts strand = 
-  M.fromList (map (\nuc -> (nuc, (count nuc strand))) "GATC")
+  M.fromList $ map buildCount "GATC"
+  where
+    buildCount = \nuc -> (nuc, count nuc strand)
