918155b5ab0c04a9169d3c47fefafc9d53a402e7
Nat Williams <nat.williams@gmail.com>
2014-05-12 07:11:05 -0500

    use Data.List.Split.wordsBy

--- haskell/word-count/WordCount.hs
+++ haskell/word-count/WordCount.hs
@@ -1,19 +1,18 @@
 module WordCount (wordCount) where
 
-  import qualified Data.Map.Strict as M
-  import Data.Char
-  import Data.List (foldl')
-  import Data.List.Split (splitWhen)
+import qualified Data.Map.Strict as M
+import Data.Char
+import Data.List (foldl')
+import Data.List.Split (wordsBy)
 
-  wordCount :: String -> (M.Map String Int)
-  wordCount = 
-    foldl' predicate M.empty . splitAndClean
-    where predicate = (\acc word -> M.insertWith (+) word 1 acc)
+wordCount :: String -> (M.Map String Int)
+wordCount =
+  foldl' countWord M.empty . splitAndClean
+  where countWord = (\acc word -> M.insertWith (+) word 1 acc)
 
-  splitAndClean :: String -> [String]
-  splitAndClean = 
-    stripEmpty . split . lower
-    where
-      stripEmpty = filter (not . null)
-      split = splitWhen $ not . isAlphaNum
-      lower = fmap toLower
+splitAndClean :: String -> [String]
+splitAndClean =
+  split . lower
+  where
+    split = wordsBy $ not . isAlphaNum
+    lower = fmap toLower
