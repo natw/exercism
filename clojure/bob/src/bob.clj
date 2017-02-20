164697c8430e33f8488eb2f2072d1cffd591fe15
Nathaniel Williams <nat@livewatch.com>
2017-02-20 17:38:30 -0600

    clojure/bob

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,6 +1,10 @@
 (ns bob)
 
 (defn response-for
-  ([msg] "Whatever.")
-  ([] "Whatever.")
+  [msg]
+  (cond
+    (re-matches #"^\s*$" msg) "Fine. Be that way!"
+    (re-matches #"^(?:(?![a-z]+).)+[A-Z](?:(?![a-z]+).)+$" msg) "Whoa, chill out!"
+    (re-matches #".*\?$" msg) "Sure."
+    :else "Whatever.")
 )
