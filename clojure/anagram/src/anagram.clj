ae6099b4161a2a558c671fafde29e8c74fd4cac3
Nathaniel Williams <nat@livewatch.com>
2017-02-22 08:54:12 -0600

    clojure anagram

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,11 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as string]))
 
-(defn anagrams-for [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn are-anagrams
+  [w1 w2] (let [lw1 (string/lower-case w1) lw2 (string/lower-case w2)]
+            (and (= (sort lw1) (sort lw2))
+                 (not= lw1 lw2))))
+
+(defn anagrams-for
+  [word, candidates]
+  (filter (partial are-anagrams word) candidates))
