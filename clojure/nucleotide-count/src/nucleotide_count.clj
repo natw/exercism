94bc66ee6f5f2b4ea36f752fdc116d1901b2a673
Nathaniel Williams <nat@livewatch.com>
2017-02-23 10:11:05 -0600

    clojure nucleotides

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -2,12 +2,10 @@
 
 (defn count
   ([nucleotide dna]
-   (reduce (fn [cnt chr] (cond (= chr nucleotide) (+ cnt 1) :else cnt)) 0 dna))
+   (cond (not (contains? #{\G \A \T \C} nucleotide)) (throw (Throwable. (str nucleotide "not a nucleotide"))))
+   (reduce (fn [cnt chr] (cond (= chr nucleotide) (+ cnt 1) :else cnt)) 0 dna)))
+
+(defn nucleotide-counts
   ([dna]
    (let [start {\A 0 \T 0 \C 0 \G 0}]
      (reduce (fn [cnt chr] (update cnt chr inc)) start dna))))
-
-(count "GATCC")
-
-
-(count \A "")
